357dfcf4044c1c44d0c40ecd88c43a470d5d8195
FiveEight <55926138+an0tv@users.noreply.github.com>
2022-07-22 13:13:52 -0700

    Clarification on main
    Added a comment to clarify the existence of main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-
+//All this is  a main class that I used as a way to test and see if otehr classes work properly, but now it is used to run the program. 
 public class main{
     public static void main(String[] args) throws IOException {
         UserInterface test = new UserInterface();
